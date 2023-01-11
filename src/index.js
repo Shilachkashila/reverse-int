7edd4e90b2ce5658b3ce470131a23523975a33ba
Shilachkashila <67004521+Shilachkashila@users.noreply.github.com>
2023-01-11 21:15:25 +0200

    start and finish

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+  n = Math.abs(n);
+  n = String(n);
+  let mirroredN = "";
+  for (let i = n.length - 1; i >= 0; i--) {
+    mirroredN += n[i];
+  }
+return Number(mirroredN);
 }
